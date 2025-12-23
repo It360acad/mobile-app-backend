be13f64ee573adfaa94d8e120cccf17667a2f123
hisreal123 <omosgoerge@gmail.com>
2025-12-23 11:05:27 +0100

    delete user

---
+++ gunicorn_config.py
@@ -0,0 +1,38 @@
+# Gunicorn configuration file
+import multiprocessing
+import os
+
+# Server socket
+# Note: bind is set explicitly in Procfile as --bind 0.0.0.0:$PORT
+# This ensures Render can detect the open port
+# Do NOT set bind here to avoid conflicts
+backlog = 2048
+
+# Worker processes
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = "sync"
+worker_connections = 1000
+timeout = 30
+keepalive = 2
+
+# Logging
+accesslog = "-"  # Log to stdout
+errorlog = "-"   # Log to stderr
+loglevel = "info"
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+
+# Process naming
+proc_name = "it360acad_backend"
+
+# Server mechanics
+daemon = False
+pidfile = None
+umask = 0
+user = None
+group = None
+tmp_upload_dir = None
+
+# SSL (uncomment if using SSL)
+# keyfile = None
+# certfile = None
+
--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Startup script for Render deployment
 # Ensures gunicorn binds to 0.0.0.0 for Render port detection
+# This script MUST be used as the Start Command in Render dashboard
 
 set -o errexit
 
@@ -8,7 +9,8 @@ set -o errexit
 PORT="${PORT:-8000}"
 
 # Start gunicorn with explicit 0.0.0.0 binding
+# The --bind parameter MUST come last to override any Render-added bind settings
 exec gunicorn it360acad_backend.wsgi:application \
-    --bind "0.0.0.0:${PORT}" \
-    --config gunicorn_config.py
+    --config gunicorn_config.py \
+    --bind "0.0.0.0:${PORT}"
 
